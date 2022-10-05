22dccc1cc32be21d6cbd42b7785625dc9b36f32d
Camila Pinzon <camilapinz18@gmail.com>
2022-10-05 12:53:14 -0500

    ejercicio terminado sin css

---
+++ ejercicio456/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './components/Clock';
+import ClockHook from './components/ClockHook';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/*<Clock></Clock>*/}
+        <ClockHook></ClockHook>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ejercicio456/src/components/ClockHook.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+//import ‘../../styles/clock.scss’;
+
+const ClockHook = () => {
+
+    //Definir estado inicial:
+    const initialState = {
+        fecha: new Date(),
+        edad: 0,
+        nombre: "Camila",
+        apellidos: "Pinzón"
+    }
+    //Definir el useState
+    const [state, setState] = useState(initialState);
+
+    //Definir funcion que modifica el estado con cada llamada de setInterval
+    function tick() {
+        setState((state) => {
+            let edad = state.edad + 1;
+            return {
+                ...state,
+                fecha: new Date(),
+                edad
+            }
+        });
+    }
+
+    useEffect(() => {
+        const timerID = setInterval(
+            () => tick(), 1000
+        );
+        return () => {
+            clearInterval(timerID);
+        }
+    }, []);
+
+    return (
+        <div>
+            <h2>
+                Hora Actual:
+                {state.fecha.toLocaleTimeString()}
+            </h2>
+            <h3>{state.nombre} {state.apellidos}</h3>
+            <h1>Edad: {state.edad}</h1>
+        </div>
+    )
+}
+
+export default ClockHook;
